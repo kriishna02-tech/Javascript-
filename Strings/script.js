a438d0225878ef3ff3d8033dbb510673c1af6627
Krishna <kriishna002@gmail.com>
2026-07-12 13:18:22 +0530

    string part complete

---
+++ Strings/script.js
@@ -0,0 +1,16 @@
+//Print the length of a string.
+
+let name = prompt("enter string: ");
+console.log(name.length); // it will also count space ,special character
+
+//Convert a string to uppercase.
+console.log(name.toUpperCase());
+
+//Convert a string to lowercase.
+console.log(name.toLowerCase());
+
+// Extract the first 5 characters from a string.
+console.log(name.substring(0,5));
+
+//Replace "Java" with "Type" in "JavaScript".
+console.log(name.replace("java" ,"type"));
